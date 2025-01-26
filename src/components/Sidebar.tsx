37f1caede5acf678be887c9071faa59e3721bcba
Garenaaq <GlebSpoke@gmail.com>
2025-01-26 20:51:27 +0300

    fix sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,11 +3,17 @@ import React from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 import { Button } from './ui/button';
 import { SignInButton, SignUpButton } from '@clerk/nextjs';
+import { getUserByClerkId } from '@/actions/user.actions';
 
 async function Sidebar() {
   const authUser = await currentUser();
 
   if (!authUser) return <UnAuthenticatedSidebar />;
+
+  const user = await getUserByClerkId(authUser.id);
+  if (!user) return null;
+
+
 
   return (
     <div>Sidebar</div>
