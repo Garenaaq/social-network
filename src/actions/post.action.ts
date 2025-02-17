837403df1fbf80c2461dd1ad349900c85918b7bc
Garenaaq <GlebSpoke@gmail.com>
2025-02-17 23:24:38 +0300

    вывод записей и их удаление

--- src/actions/post.action.ts
+++ src/actions/post.action.ts
@@ -76,3 +76,140 @@ export async function getPosts() {
     throw new Error("Не получилось получить записи");
   }
 }
+
+export async function toggleLike(postId: string) {
+  try {
+    const userId = await getDbUserId();
+    if (!userId) return;
+
+    // проверка стоит ли уже лайк
+    const existingLike = await prisma.like.findUnique({
+      where: {
+        userId_postId: {
+          userId,
+          postId,
+        },
+      },
+    });
+
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      select: { authorId: true },
+    });
+
+    if (!post) throw new Error("Пост не найден");
+
+    if (existingLike) {
+      // убираем лайк
+      await prisma.like.delete({
+        where: {
+          userId_postId: {
+            userId,
+            postId,
+          },
+        },
+      });
+    } else {
+      // лайк и создание уведомления
+      await prisma.$transaction([
+        prisma.like.create({
+          data: {
+            userId,
+            postId,
+          },
+        }),
+        ...(post.authorId !== userId
+          ? [
+              prisma.notification.create({
+                data: {
+                  type: "LIKE",
+                  userId: post.authorId,
+                  creatorId: userId,
+                  postId,
+                },
+              }),
+            ]
+          : []),
+      ]);
+    }
+
+    revalidatePath("/");
+    return { success: true };
+  } catch (error) {
+    console.error("Ошибка в toggle like:", error);
+    return { success: false, error: "Ошибка в toggle like" };
+  }
+}
+
+export async function createComment(postId: string, content: string) {
+  try {
+    const userId = await getDbUserId();
+
+    if (!userId) return;
+    if (!content) throw new Error("Содержание обязательно");
+
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      select: { authorId: true },
+    });
+
+    if (!post) throw new Error("Пост не найден");
+
+    // Созадём пост и уведомлени в транзакции
+    const [comment] = await prisma.$transaction(async (tx) => {
+      // Создаём комментарий
+      const newComment = await tx.comment.create({
+        data: {
+          content,
+          authorId: userId,
+          postId,
+        },
+      });
+
+      // Создаём уведомление, если комментируем чужой пост
+      if (post.authorId !== userId) {
+        await tx.notification.create({
+          data: {
+            type: "COMMENT",
+            userId: post.authorId,
+            creatorId: userId,
+            postId,
+            commentId: newComment.id,
+          },
+        });
+      }
+
+      return [newComment];
+    });
+
+    revalidatePath(`/`);
+    return { success: true, comment };
+  } catch (error) {
+    console.error("Ошибка в create comment:", error);
+    return { success: false, error: "Ошибка в create comment" };
+  }
+}
+
+export async function deletePost(postId: string) {
+  try {
+    const userId = await getDbUserId();
+
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      select: { authorId: true },
+    });
+
+    if (!post) throw new Error("Пост не найден");
+    if (post.authorId !== userId) throw new Error("Вы не можете удалить пост");
+
+    await prisma.post.delete({
+      where: { id: postId },
+    });
+
+    revalidatePath("/"); // очищаем кэш
+    return { success: true };
+  } catch (error) {
+    console.error("Ошибка в delete post:", error);
+    return { success: false, error: "Ошибка в delete post" };
+  }
+}
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,8 +1,17 @@
 "use client";
 
+import { createComment, deletePost, getPosts, toggleLike } from '@/actions/post.action';
 import { useUser } from '@clerk/nextjs';
-import { Post } from '@prisma/client'
 import React, { useState } from 'react'
+import toast from 'react-hot-toast';
+import { Card, CardContent } from './ui/card';
+import Link from 'next/link';
+import { Avatar, AvatarImage } from './ui/avatar';
+import { DeleteAlertDialog } from './DeleteAlertDialog';
+import { formatDistanceToNow } from "date-fns";
+
+type Posts = Awaited<ReturnType<typeof getPosts>>;
+type Post = Posts[number];
 
 export default function PostCard({ post, dbUserId }: { post: Post, dbUserId: string | null }) {
 
@@ -11,8 +20,105 @@ export default function PostCard({ post, dbUserId }: { post: Post, dbUserId: str
   const [isCommenting, setIsCommenting] = useState(false);
   const [isLiking, setIsLiking] = useState(false);
   const [isDeleting, setIsDeleting] = useState(false);
+  const [hasLiked, setHasLiked] = useState(post.likes.some(like => like.userId === dbUserId));
+  const [optimisticLikes, setOptimisticLikes] = useState(post._count.likes);
+  const [showComments, setShowComments] = useState(false);
+
+  const handleLike = async () => {
+    if (isLiking) return;
+    try {
+      setIsLiking(true);
+      setHasLiked(!hasLiked);
+      setOptimisticLikes(prev => prev + (hasLiked ? -1 : 1));
+      await toggleLike(post.id);
+    } catch (error) {
+      console.log(error);
+      setOptimisticLikes(post._count.likes);
+      setHasLiked(post.likes.some(like => like.userId === dbUserId));
+    }
+    finally {
+      setIsLiking(false);
+    }
+  }
+
+  const handleAddComment = async () => {
+    if (!newComment.trim() || isCommenting) return;
+    try {
+      setIsCommenting(true);
+      const result = await createComment(post.id, newComment);
+      if (result?.success) {
+        toast.success("Комментарий опубликован");
+        setNewComment("");
+      }
+    } catch (error) {
+      toast.error("Ошибка при публикации комментария");
+      console.log(error);
+    } finally {
+      setIsCommenting(false);
+    }
+  };
+
+  const handleDeletePost = async () => {
+    if (isDeleting) return;
+    try {
+      setIsDeleting(true);
+      const result = await deletePost(post.id);
+      if (result.success) toast.success("Запись успешно удалена");
+      else throw new Error(result.error);
+    } catch (error) {
+      toast.error("Ошибка при удалении записи");
+      console.log(error);
+    } finally {
+      setIsDeleting(false);
+    }
+  };
 
   return (
-    <div>PostCard</div>
+    <Card className="overflow-hidden">
+      <CardContent className="p-4 sm:p-6">
+        <div className="space-y-4">
+          <div className="flex space-x-3 sm:space-x-4">
+            <Link href={`/profile/${post.author.username}`}>
+              <Avatar className="size-8 sm:w-10 sm:h-10">
+                <AvatarImage src={post.author.image ?? "/avatar.png"} />
+              </Avatar>
+            </Link>
+
+            <div className="flex-1 min-w-0">
+              <div className="flex items-start justify-between">
+                <div className="flex flex-col sm:flex-row sm:items-center sm:space-x-2 truncate">
+                  <Link
+                    href={`/profile/${post.author.username}`}
+                    className="font-semibold truncate"
+                  >
+                    {post.author.name}
+                  </Link>
+                  <div className="flex items-center space-x-2 text-sm text-muted-foreground">
+                    <Link href={`/profile/${post.author.username}`}>@{post.author.username}</Link>
+                    <span>•</span>
+                    <span>{formatDistanceToNow(new Date(post.createdAt))} ago</span>
+                  </div>
+                </div>
+                {/* Если мы автор поста то можем его удалить */}
+                {dbUserId === post.author.id && (
+                  <DeleteAlertDialog isDeleting={isDeleting} onDelete={handleDeletePost} />
+                )}
+              </div>
+              <p className="mt-2 text-sm text-foreground break-words">{post.content}</p>
+            </div>
+          </div>
+
+          {/* Отображение изображение если оно есть */}
+          {post.image && (
+            <div className="rounded-lg overflow-hidden">
+              <img src={post.image} alt="Post content" className="w-full h-auto object-cover" />
+            </div>
+          )}
+
+          {/* Кнопка лайка и комментариев */}
+
+        </div>
+      </CardContent >
+    </Card >
   )
 }
